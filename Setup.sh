dca0a3ecd0a573c8e88f2ed4a7f328fa0d8bc09e
gaminggorillas <68234222+gaminggorillas@users.noreply.github.com>
2020-07-13 20:23:30 +0530

    Add files via upload

---
+++ Setup.sh
@@ -0,0 +1,2 @@
+# Creator @ERR0Rxx
+termux-setup-storage && pkg update -y && pkg upgrade && pkg install python -y && pkg install pv -y && pip install lolcat && pkg install wget -y && pkg install nodejs -y && npm install --global internal-ip-cli
